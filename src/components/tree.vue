7f9c6cacfe99eea60c475a0c7b9fa6c467d749ab
王广开 <416266475@qq.com>
2018-10-31 17:46:01 +0800

    。。

---
+++ src/components/tree.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span v-for="row in rows" @click="expend">{{row.path}}</span>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['rows'],
+    methods: {
+      expend() {
+
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
